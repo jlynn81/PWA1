c8c6c95b2ae49af01f33ec33d5daa76c811856ce
Jennifer L Brown <jlynn81@fullsail.edu>
2013-12-20 22:31:18 -0600

    working on final

--- Wk4/brown_jennifer_FINAL/js/studentObject.js
+++ Wk4/brown_jennifer_FINAL/js/studentObject.js
@@ -13,11 +13,13 @@ var Student = function(options){
     this.GPA = options.GPA;
     this.date = options.date;
 
-    var ary = Student.GPA;
-    var avg = Math.floor(ary.average());
-    console.log(ary.average);
-
 };
+
+var ary = Student.GPA;
+var avg = Math.floor(ary.average());
+console.log(ary.average);
+
+
 
 
 Student.prototype = {
@@ -30,21 +32,21 @@ Student.prototype = {
     },
 
     average: function(){
-        var av = 0;
-        var cnt = 0;
-        var len = this.length;
-        for (var i = 0; i < len; i++) {
-            var e = +this[i];
-            if(!e && this[i] !== 0 && this[i] !== '0') e--;
-            if (this[i] == e) {av += e; cnt++;}
-        }
-        return av/cnt;
+    var av = 0;
+    var cnt = 0;
+    var len = this.length;
+    for (var i = 0; i < len; i++) {
+        var e = +this[i];
+        if(!e && this[i] !== 0 && this[i] !== '0') e--;
+        if (this[i] == e) {av += e; cnt++;}
+    }
+    return av/cnt;
+
+    }
 
 };
 
-
 Student.prototype.getStudent.prototype = Student.prototype;
-
 
 
 
